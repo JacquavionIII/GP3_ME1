e07f741354fb108373f8faf648571cfe4b5ef3ec
gingeninge <sengoara.lethabo@gmail.com>
2025-08-18 11:53:39 +0200

    player spawn works now
    spawn works

---
+++ Squabble Up/Assets/Scripts/Game Manager/PlayerSpawn.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    public Transform[] SpawnPoints;
+    private int playerCount;
+
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        playerInput.transform.position = SpawnPoints[playerCount].transform.position;
+        playerCount++;
+    }
+}
